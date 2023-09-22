383b44e52db48ce762b6856f01c8166e8f154fff
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2023-09-22 17:18:53 +0200

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-05-movies">
     <App />
     <ToastContainer autoClose={1000}/>
   </BrowserRouter>
